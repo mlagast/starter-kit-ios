6b393864a5f4bcea708c39448216d8ebae0fd647
Matthieu Lagast <matthieu.lagast@gmail.com>
2020-05-31 18:17:28 +0200

    initial commit

---
+++ StarterKit/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  StarterKit
+//
+//  Created by Matthieu Lagast on 31/05/2020.
+//  Copyright © 2020 mlagast. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StarterKit/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StarterKit
+//
+//  Created by Matthieu Lagast on 31/05/2020.
+//  Copyright © 2020 mlagast. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
